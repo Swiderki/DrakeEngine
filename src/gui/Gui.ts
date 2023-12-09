b4e6b1f8ac1d8cc71e150bf8135fcce2c31b95e7
MrFishPL <karpik-junior@o2.pl>
2023-12-10 00:56:37 +0100

    Code review Some changes in the classes' logic A lot of comments

--- src/gui/Gui.ts
+++ src/gui/Gui.ts
@@ -1,20 +1,25 @@
 // Klasa gui, jedno gui do sceny
+
+import GuiElement from "./guiElement";
+
+// I didn't touch it very much so it may require more review
+// I will work on it tomorrow
+
 // gui element (width, position, interface)
-export default class GUI{
-    elements: [];
-    
-    constructor() {
-        this.elements = [];
-        this.addEventListeners();
-    }
-    renderGui() {
+export default class GUI {
+  elements: Array<GuiElement> = [];
 
-    }
-    handleEvent(){
+  constructor() {
+    this.addEventListeners();
+  }
 
-    }
-    addEventListeners(){
+  // Render logic is implemented in Engine class
+  // For example GameObject does not have render function
+  // So it shoud not have also
+  // renderGui() {  }
+  handleEvent() {}
 
-    }
-
+  // It shoud be private
+  // Using it by the user may couse some unexpected errors
+  private addEventListeners() {}
 }
--- src/gui/guiElement.ts
+++ src/gui/guiElement.ts
@@ -1,54 +1,120 @@
 import GUI from "./Gui";
 
-interface GuiElement extends GUI{
-    get width(): number;
-    set width(value: number);
+export default interface GuiElement {
+  // It don't have to be getters and setters in the interface
+  // Because you can implement them that way if you want
+  // Example in Text class
+  // You can translate "Interfase" as "Międzymordzie" btw
+  height: number;
+  width: number;
+  position: { x: number; y: number };
 
-    get height(): number;
-    set height(value: number);
+  // Every element has its own rendering logic so it has to implement render function
+  // But render function must get ctx value as a parameter to draw on
+  render(ctx: CanvasRenderingContext2D): void;
 
-    get position(): { x: number, y: number };
-    set position(value: { x: number, y: number });
-
-    render(): void;
-    onClick(): void;
-    onHover(): void;
+  // Only buttons shoud have these methods
+  // You don't want to click on text
+  // You can make button look like a simple text but that will still be a button
+  // onClick(): void;
+  // onHover(): void;
 }
 
-class Button extends GUI implements GuiElement{
-    private _width: number = 100;
-    private _height: number = 50;
-    private _position: { x: number, y: number } = {x: 0, y: 0};
-    get width(): number {
-        return this._width;
-    }
+export class Text implements GuiElement {
+  text: string;
+  fontSize: number;
+  fontFamily: string;
+  fontWeight: number;
+  position: { x: number; y: number } = { x: 0, y: 0 };
+  protected _width: number | undefined;
+  protected _height: number | undefined;
+  constructor(text: string, fontSize: number, fontFamily: string, fontWeight: number = 400) {
+    this.text = text;
+    this.fontSize = fontSize;
+    this.fontFamily = fontFamily;
+    this.fontWeight = fontWeight;
+  }
 
-    set width(value: number) {
-        this._width = value;
-    }
+  // You implement height width and position as getters and setters
+  // A programmer can decide how they wants it
+  // In text, we have to return this._width or calculate this value if this._width is unset
+  // Or maybe its better to calculate this ones, in constructor?
+  // Idk
+  get width(): number {
+    throw new Error("Method not implemented.");
+  }
+  set width(value: number) {
+    throw new Error("Method not implemented.");
+  }
+  get height(): number {
+    throw new Error("Method not implemented.");
+  }
+  set height(value: number) {
+    throw new Error("Method not implemented.");
+  }
 
-    get height(): number {
-        return this._height;
-    }
+  render(ctx: CanvasRenderingContext2D): void {
+    throw new Error("Method not implemented.");
+  }
+}
 
-    set height(value: number) {
-        this._height = value;
-    }
+// extends GUI was there
+// I don't get why if GUI is like container for GuiElements
+export class Button extends Text implements GuiElement {
+  override position: { x: number; y: number } = { x: 0, y: 0 };
 
-    get position(): { x: number, y: number } {
-        return this._position;
-    }
+  // There are some objects having top, bottom, left and right value
+  // Maybe it is a good idea to implement a type for them.
+  border: {
+    top: { color: string; width: number };
+    bottom: { color: string; width: number };
+    left: { color: string; width: number };
+    right: { color: string; width: number };
+  } = {
+    top: { color: "#fff", width: 1 },
+    bottom: { color: "#fff", width: 1 },
+    left: { color: "#fff", width: 1 },
+    right: { color: "#fff", width: 1 },
+  };
 
-    set position(value: { x: number, y: number }) {
-        this._position = value;
-    }
-    render(){
-        
-    }
-    onClick(): void{
+  padding: {
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  } = {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  };
 
-    }
-    onHover(): void {
-        
-    }
+  override get width(): number {
+    // If _width is undefined, we have to calculate it using super.width, padding etc
+    if (this._width == undefined) return 0;
+    return this._width;
+  }
+
+  override set width(value: number) {
+    this._width = value;
+  }
+
+  override get height(): number {
+    // If _height is undefined, we have to calculate it using super.height, padding etc
+    if (this._height == undefined) return 0;
+    return this._height;
+  }
+
+  override set height(value: number) {
+    this._height = value;
+  }
+
+  constructor(text: string, fontSize: number, fontFamily: string, fontWeight: number = 400) {
+    super(text, fontSize, fontFamily, fontWeight);
+  }
+
+  override render(ctx: CanvasRenderingContext2D) {}
+
+  onClick(): void {}
+  onHover(): void {}
 }
