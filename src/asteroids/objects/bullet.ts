ef7c705158222c152f1fe5502216aead7e9aeea7
karolciurej <101570801+karolciurej@users.noreply.github.com>
2024-02-05 19:49:01 +0100

    started working with bullets

--- src/asteroids/objects/bullet.ts
+++ src/asteroids/objects/bullet.ts
@@ -1,8 +1,12 @@
 import GameObject from "@/src/entities/game-objects/GameObject";
+import PhysicalObject from "@/src/entities/game-objects/PhysicalObject";
 
-export default class Bullet extends GameObject {
+export default class Bullet extends PhysicalObject {
   constructor(position?: Vec3DTuple, size?: Vec3DTuple, rotation?: Vec3DTuple) {
     super(`src/asteroids/objects/obj/bullet.obj`, { position, size, rotation });
+  }
+  override updatePhysics(deltaTime: number): void {
+    super.updatePhysics(deltaTime);
   }
 }
 
--- src/usage.ts
+++ src/usage.ts
@@ -164,6 +164,10 @@ class MyGame extends Drake.Engine {
       }, 700);
 
     }
+    if (this.keysPressed.has("k")) {
+
+    }
+
 
   }
 
@@ -172,14 +176,12 @@ class MyGame extends Drake.Engine {
     if (e.key == "w") {
       this.flame.obj.setPosition(this.spaceship.obj.position.x, this.spaceship.obj.position.y, this.spaceship.obj.position.z)
     }
-    console.log(this.flame.obj.position)
     this.handleSpaceshipMove();
 
   }
 
   handleKeyUp(e: KeyboardEvent) {
     this.keysPressed.delete(e.key);
-    console.log(this.flame.obj.position)
 
     if (e.key == "w") {
       this.flame.obj.setPosition(1231231231, 123123123, 123123123)
@@ -195,13 +197,13 @@ class MyGame extends Drake.Engine {
     const camera = new Drake.Camera(60, 0.1, 1000, [0, 0, -10], [0, 0, 1]);
 
     const mainScene = new Drake.Scene(this.width, this.height);
-    const svgPath = "M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1 c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3 l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4 C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3 s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4 c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3 C444.801,187.101,434.001,213.101,414.401,232.701z"
+    const svgPath = "m 10 0 l 10 40 l -3 -5 l -14 0 l -3 5 z"
     const mainSceneGUI = new GUI(this.getCanvas, this.getCanvas.getContext("2d")!);
     const resultText = new GUIText("00", 35, "Arial", "white", 100);
     const bestResultText = new GUIText("00", 35, "Arial", "white", 100);
-    const icon1 = new Icon(svgPath, 70, 70, { x: 230, y: 60 }, "white");
-    const icon2 = new Icon(svgPath, 70, 70, { x: 255, y: 60 }, "white");
-    const icon3 = new Icon(svgPath, 70, 70, { x: 280, y: 60 }, "white");
+    const icon1 = new Icon(svgPath, 770, 770, { x: 245, y: 60 });
+    const icon2 = new Icon(svgPath, 770, 770, { x: 265, y: 60 });
+    const icon3 = new Icon(svgPath, 770, 770, { x: 285, y: 60 });
 
     resultText.position = { x: 250, y: 30 };
     bestResultText.position = { x: 600, y: 30 };
