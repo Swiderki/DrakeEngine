7e9cf2a2795870608fb896782b5311d26086acc0
Kejczorek <beka89t@gmail.com>
2024-02-12 22:22:32 +0100

    build: prepare for npm, move asteroids to other branch

--- src/asteroids/objects/bullet.ts
+++
@@ -1,8 +0,0 @@
-import GameObject from "@/src/entities/game-objects/GameObject";
-
-export default class Bullet extends GameObject {
-  constructor(position?: Vec3DTuple, size?: Vec3DTuple, rotation?: Vec3DTuple) {
-    super(`src/asteroids/objects/obj/bullet.obj`, { position, size, rotation });
-  }
-}
-
--- src/asteroids/objects/ufo.ts
+++
@@ -1,7 +0,0 @@
-import GameObject from "@/src/entities/game-objects/GameObject";
-
-export default class Ufo extends GameObject {
-    constructor(position?: Vec3DTuple, size?: Vec3DTuple, rotation?: Vec3DTuple) {
-      super(`src/asteroids/objects/obj/ufo.obj`, { position, size, rotation });
-    }
-  }
