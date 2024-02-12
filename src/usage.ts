b1141bb4843514b63c5610ccb318b8a66f2a5a2d
Zwx <staszekd2020@gmail.com>
2024-02-12 11:15:00 +0100

    fixed usage

--- src/usage.ts
+++ src/usage.ts
@@ -1,88 +1,112 @@
-import GameObject from "./GameObject";
-import { Vector } from "@/src/util/math";
+import { Overlap } from "./behavior/Overlap";
+import PhysicalObject from "./entities/game-objects/PhysicalObject";
+import Cube from "./entities/game-objects/built-in/Cube";
+import Drake from "./index";
 
-export default class PhysicalObject extends GameObject {
-  velocity: Vec3D = { x: 0, y: 0, z: 0 };
-  acceleration: Vec3D = { x: 0, y: 0, z: 0 };
-  mass: number;
+import { QuaternionUtils } from "@/src/util/quaternions";
+import { Vector } from "./util/math";
+import IdGenerator from "./util/idGenerator";
 
-  constructor(
-    meshPath: string,
-    {
-      allowUsingCachedMesh,
-      position,
-      rotation,
-      size,
-      velocity = { x: 0, y: 0, z: 0 },
-      acceleration = { x: 0, y: 0, z: 0 },
-      mass = 1,
-    }: PhysicalObjectInitialConfig
-  ) {
-    super(meshPath, { allowUsingCachedMesh, position, rotation, size });
-    this.velocity = velocity;
-    this.acceleration = acceleration;
-    this.mass = mass;
+const canvas = document.getElementById("app") as HTMLCanvasElement | null;
+if (!canvas) throw new Error("unable to find canvas");
+
+class MyOverlap extends Overlap {
+  override onOverlap(): void {
+    console.log("Dzieje się!");
   }
+}
 
-  // Method to update the object's position based on velocity and acceleration
-  updatePhysics(deltaTime: number): void {
-    console.log(new Date().getTime());
-    const deltaVelocity = Vector.multiply(this.acceleration, deltaTime);
-    this.move(
-      this.velocity.x * deltaTime + 0.5 * deltaVelocity.x * deltaTime,
-      this.velocity.y * deltaTime + 0.5 * deltaVelocity.y * deltaTime,
-      this.velocity.z * deltaTime + 0.5 * deltaVelocity.z * deltaTime
-    );
+class MyGame extends Drake.Engine {
+  cube: Cube;
+  axis;
+  hue: number = 0;
+  vec: number = 1;
+  cubes: Cube[] = [];
+  physicalCube: PhysicalObject;
+  rotationQuaternion = { x: 0, y: 0, z: 0, w: 1 };
+  constructor(canvas: HTMLCanvasElement) {
+    super(canvas);
+    this.cube = new Drake.Cube([10, 10, -14]);
+    this.axis = new Drake.Piramide();
 
-    // Update velocity with the acceleration for the next frame
-    this.velocity = Vector.add(this.velocity, deltaVelocity);
-  }
+    const c1 = new Cube([0, 0, 0], [1, 1, 1]);
+    const c2 = new Cube([5, 0, 0], [1, 1, 1]);
 
-  // Method to apply a force to the object
-  applyForce(force: Vec3D): void {
-    const deltaAcceleration = Vector.divide(force, this.mass); // Assuming mass is a property of GameObject
-    this.acceleration = Vector.add(this.acceleration, deltaAcceleration);
-  }
+    c1.boxCollider = [
+      { x: 0, y: 0, z: 0 },
+      { x: 1, y: 1, z: 1 },
+    ];
+    c2.boxCollider = [
+      { x: 0, y: 0, z: 0 },
+      { x: 1, y: 1, z: 1 },
+    ];
 
-  static createFromGameObject(
-    gameObject: GameObject,
-    initialConfig?: PhysicalObjectInitialConfig
-  ): PhysicalObject {
-    const {
-      position = [
-        gameObject.position.x,
-        gameObject.position.y,
-        gameObject.position.z,
-      ],
-      rotation = [
-        gameObject.rotation.xAxis,
-        gameObject.rotation.yAxis,
-        gameObject.rotation.zAxis,
-      ],
-      size = [gameObject.size.x, gameObject.size.y, gameObject.size.z],
-      velocity = { x: 0, y: 0, z: 0 },
-      acceleration = { x: 0, y: 0, z: 0 },
-      mass = 1,
-    } = initialConfig || {};
-
-    return new PhysicalObject(gameObject.meshPath, {
-      allowUsingCachedMesh: gameObject.allowUsingCachedMesh,
-      position: position,
-      rotation: rotation,
-      size: size,
-      velocity,
-      acceleration,
-      mass,
+    this.cubes.push(c1);
+    this.cubes.push(c2);
+    // this.physicalCube = new PhysicalObject("objects/cube_wire.obj", {position: [0, 3, 0]});
+    this.physicalCube = PhysicalObject.createFromGameObject(new Cube([0, 3, 0]), {
+      // acceleration: { x: 0, y: 1, z: 0 },
     });
   }
+
+  handleCameraMove(e: KeyboardEvent) {
+    if (!this.mainCamera) return;
+    if (e.key === "w") this.mainCamera.move(0, 1, 0);
+    if (e.key === "s") this.mainCamera.move(0, -1, 0);
+    if (e.key === "a") this.mainCamera.move(-1, 0, 0);
+    if (e.key === "d") this.mainCamera.move(1, 0, 0);
+    if (e.key === "q") this.mainCamera.rotate({x: 0, y: 1, z: 0}, Math.PI / 180 * -5);
+    if (e.key === "e") this.mainCamera.rotate({x: 0, y: 1, z: 0}, Math.PI / 180 * 5);
+  }
+
+  override Start(): void {
+    this.setResolution(1280, 720);
+    const camera = new Drake.Camera(60, 0.1, 1000, [10, 3, -15], [0, 0, 1]);
+
+    const mainScene = new Drake.Scene(this.width, this.height, IdGenerator.new());
+
+    mainScene.setCamera(camera);
+
+    const mainSceneId = this.addScene(mainScene);
+    this.setCurrentScene(mainSceneId);
+
+    this.cubes.forEach((cube) => mainScene.addSceneMesh(cube));
+    mainScene.addSceneMesh(this.physicalCube);
+    mainScene.addSceneMesh(this.axis);
+    // this.physicalCube.applyForce({x: 5, y: 0, z: 0});
+    // this.physicalCube.velocity = Vector.fromArray([8, 0, 0]);
+
+    const ov = new MyOverlap(this.cubes[0], this.cubes[1]);
+    mainScene.addOverlap(ov);
+
+    this.currentScene.animatedObjectDestruction(this.physicalCube);
+
+    this.setResolution(640, 480);
+    document.addEventListener("keydown", this.handleCameraMove.bind(this));
+  }
+
+  override Update(): void {
+    const rotationSpeed = Math.PI / 2; // Obrót o 360 stopni na sekundę
+
+    // Aktualizacja kwaternionu rotacji
+    QuaternionUtils.setFromAxisAngle(
+      this.rotationQuaternion,
+      { x: 0, y: 1, z: 0 }, // Oś obrotu
+      rotationSpeed * this.deltaTime // Kąt obrotu
+    );
+
+    // Normalizacja kwaternionu
+    QuaternionUtils.normalize(this.rotationQuaternion);
+
+    this.physicalCube.updatePhysics(this.deltaTime);
+
+    // Zastosowanie kwaternionu do obrotu kostek, piramidy i osi
+    // this.cubes.forEach((cube) => cube.applyQuaternion(this.rotationQuaternion));
+
+    // this.cubes[1]!.move(0.2 * this.vec, 0, 0);
+    // if (this.cubes[1].position.x > 20 || this.cubes[1].position.x < -5) this.vec *= -1;
+  }
 }
 
-interface PhysicalObjectInitialConfig {
-  allowUsingCachedMesh?: boolean;
-  position?: Vec3DTuple;
-  rotation?: Vec3DTuple;
-  size?: Vec3DTuple;
-  velocity?: Vec3D;
-  acceleration?: Vec3D;
-  mass?: number;
-}
+const game = new MyGame(canvas);
+game.run();
