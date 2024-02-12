debec04d9704406591b06ac8b173b14ddebc938e
karolciurej <xdarkin1w@gmail.com>
2024-02-12 11:23:22 +0100

    added stroke color to icons

--- src/gui/GUIElements/Icon.ts
+++ src/gui/GUIElements/Icon.ts
@@ -1,21 +1,25 @@
 import GuiElement from "./GuiElement";
-
 export class Icon implements GuiElement {
   path: string;
   protected _width: number;
   protected _height: number;
   position: { x: number; y: number } = { x: 0, y: 0 };
-
+  fillColor?: string; 
+  strokeColor: string;
   constructor(
     svgPath: string,
     width: number,
     height: number,
-    position: { x: number; y: number }
+    position: { x: number; y: number },
+    strokeColor: string,
+    fillColor?: string 
   ) {
     this.path = svgPath;
     this._width = width;
     this._height = height;
     this.position = position;
+    this.fillColor = fillColor;
+    this.strokeColor = strokeColor;
   }
 
   get width(): number {
@@ -40,6 +44,12 @@ export class Icon implements GuiElement {
       this._height / ctx.canvas.height
     );
     ctx.scale(scale, scale);
+    ctx.strokeStyle = this.strokeColor;
+    if (this.fillColor) {
+      ctx.fillStyle = this.fillColor;
+      ctx.fill(path);
+    }
+
     ctx.stroke(path);
     ctx.restore();
   }
