57ee364ab9784cd28b8fb5f65680e8f631409bbf
MrFishPL <karpik-junior@o2.pl>
2024-02-13 11:48:45 +0100

    Primitive asterodis destroying

--- src/usage.ts
+++ src/usage.ts
@@ -24,8 +24,36 @@ class AsteroidPlayerOverlap extends Overlap {
   }
 
   override onOverlap(): void {
-    console.log("xd");
     if (!game.currentScene) return;
+  }
+}
+
+class BulletAsteroidOverlap extends Overlap {
+  private game: MyGame;
+  private bullet: Bullet;
+  private bulletID: number;
+  private astID: number;
+  private asteroid: Asteroid;
+  constructor(
+    obj1: Bullet,
+    obj2: Asteroid,
+    bulletID: number,
+    astID: number,
+    game: MyGame
+  ) {
+    super(obj1, obj2);
+    this.game = game;
+    this.bullet = obj1;
+    this.asteroid = obj2;
+    this.bulletID = bulletID;
+    this.astID = astID;
+  }
+
+  override onOverlap() {
+    console.log("XDDD");
+    this.game.currentScene!.killObject(this.bulletID);
+    this.game.currentScene!.killObject(this.astID);
+    this.game.asteroids.delete(this.astID);
   }
 }
 
@@ -47,12 +75,7 @@ class StartButton extends Button {
 
   override onClick(): void {
     this.game.changeScene();
-  }
-}
-
-class TestOverlap extends Overlap {
-  constructor(obj1: GameObject, obj2: GameObject) {
-    super(obj1, obj2);
+    this.game.asteroids.clear();
   }
 }
 
@@ -63,10 +86,15 @@ class MyGame extends Drake.Engine {
   GUIScene: number | null = null;
   startButton: Button | null = null;
   bullets: Bullet[] = [];
-  asteroids: Asteroid[] = [];
+  asteroids: Map<number, Asteroid> = new Map();
   keysPressed: Set<string> = new Set();
   lastAsteroidSpawnTime: number = Date.now();
-  rotationQuaternion: { x: number; y: number; z: number; w: number } = { x: 0, y: 0, z: 0, w: 1 };
+  rotationQuaternion: { x: number; y: number; z: number; w: number } = {
+    x: 0,
+    y: 0,
+    z: 0,
+    w: 1,
+  };
   flame;
 
   constructor(canvas: HTMLCanvasElement) {
@@ -74,18 +102,17 @@ class MyGame extends Drake.Engine {
     this.flame = {
       obj: new Flame([0, 0, 0], [0.1, 0.1, 0.1]),
       rotation: { x: 0, y: 0, z: 0, w: 1 },
-      id: 0
+      id: 0,
     };
     this.spaceship = {
       obj: new Spaceship([0, 0, 0], [0.1, 0.1, 0.1]),
       rotation: { x: 0, y: 0, z: 0, w: 1 },
-      id: 0
+      id: 0,
     };
     this.spaceship.obj.boxCollider = [
       { x: -0.2, y: 0.3, z: 0 },
       { x: 0.3, y: -0.3, z: -5 },
     ];
-
 
     this.spaceship.obj.showBoxcollider = true;
   }
@@ -95,7 +122,7 @@ class MyGame extends Drake.Engine {
   }
 
   createRandomAsteroid() {
-    if (this.mainScene == null) {
+    if (this.currentScene == null) {
       throw new Error("Main scene must be set first.");
     }
 
@@ -106,7 +133,9 @@ class MyGame extends Drake.Engine {
     const type = ["l", "m", "s"][Math.floor(Math.random() * 3)];
 
     // Losowanie pozycji
-    const edge = ["left", "right", "top", "bottom"][Math.floor(Math.random() * 4)];
+    const edge = ["left", "right", "top", "bottom"][
+      Math.floor(Math.random() * 4)
+    ];
     let position: [number, number, number];
     if (edge === "left") {
       position = [-18, Math.random() * 16 - 8, 0];
@@ -127,40 +156,69 @@ class MyGame extends Drake.Engine {
 
     // Losowanie i obliczanie wektora prędkości
     const velocityMagnitude = Math.random() * 6 + 3;
-    const velocityDirection = [targetPosition[0] - position[0], targetPosition[1] - position[1]];
+    const velocityDirection = [
+      targetPosition[0] - position[0],
+      targetPosition[1] - position[1],
+    ];
     const normalizedVelocity = velocityDirection.map(
-      (v) => v / Math.sqrt(velocityDirection[0] ** 2 + velocityDirection[1] ** 2)
+      (v) =>
+        v / Math.sqrt(velocityDirection[0] ** 2 + velocityDirection[1] ** 2)
     );
     const velocity = normalizedVelocity.map((v) => v * velocityMagnitude);
 
     // Tworzenie asteroidy
     const ast = new Asteroid(size, type, position, [0.1, 0.1, 0.1]);
     ast.velocity = { x: velocity[0], y: velocity[1], z: 0 };
-    this.mainScene.addSceneMesh(ast);
+    const astId = this.currentScene.addSceneMesh(ast);
 
-    const astId = this.asteroids.push(ast);
+    this.asteroids.set(astId, ast);
 
-    this.currentScene.addOverlap(new AsteroidPlayerOverlap(this.spaceship.obj, ast, this));
+    this.currentScene.addOverlap(
+      new AsteroidPlayerOverlap(this.spaceship.obj, ast, this)
+    );
   }
 
   handleSpaceshipMove() {
     const rotationAmount = Math.PI / 16;
 
     if (this.keysPressed.has("a")) {
-      QuaternionUtils.setFromAxisAngle(this.rotationQuaternion, { x: 0, y: 0, z: 1 }, rotationAmount);
-      QuaternionUtils.multiply(this.spaceship.rotation, this.rotationQuaternion, this.spaceship.rotation);
-      QuaternionUtils.multiply(this.flame.rotation, this.rotationQuaternion, this.flame.rotation);
+      QuaternionUtils.setFromAxisAngle(
+        this.rotationQuaternion,
+        { x: 0, y: 0, z: 1 },
+        rotationAmount
+      );
+      QuaternionUtils.multiply(
+        this.spaceship.rotation,
+        this.rotationQuaternion,
+        this.spaceship.rotation
+      );
+      QuaternionUtils.multiply(
+        this.flame.rotation,
+        this.rotationQuaternion,
+        this.flame.rotation
+      );
       QuaternionUtils.normalize(this.spaceship.rotation);
       QuaternionUtils.normalize(this.flame.rotation);
       this.spaceship.obj.applyQuaternion(this.rotationQuaternion);
       this.flame.obj.applyQuaternion(this.rotationQuaternion);
-
     }
 
     if (this.keysPressed.has("d")) {
-      QuaternionUtils.setFromAxisAngle(this.rotationQuaternion, { x: 0, y: 0, z: -1 }, rotationAmount);
-      QuaternionUtils.multiply(this.spaceship.rotation, this.rotationQuaternion, this.spaceship.rotation);
-      QuaternionUtils.multiply(this.flame.rotation, this.rotationQuaternion, this.flame.rotation);
+      QuaternionUtils.setFromAxisAngle(
+        this.rotationQuaternion,
+        { x: 0, y: 0, z: -1 },
+        rotationAmount
+      );
+      QuaternionUtils.multiply(
+        this.spaceship.rotation,
+        this.rotationQuaternion,
+        this.spaceship.rotation
+      );
+      QuaternionUtils.multiply(
+        this.flame.rotation,
+        this.rotationQuaternion,
+        this.flame.rotation
+      );
       QuaternionUtils.normalize(this.flame.rotation);
       QuaternionUtils.normalize(this.spaceship.rotation);
       this.spaceship.obj.applyQuaternion(this.rotationQuaternion);
@@ -171,55 +229,79 @@ class MyGame extends Drake.Engine {
       const forwardVector = { x: 0, y: 1, z: 0 };
       const direction = { x: 0, y: 0, z: 0 };
 
-      QuaternionUtils.rotateVector(this.spaceship.rotation, forwardVector, direction);
+      QuaternionUtils.rotateVector(
+        this.spaceship.rotation,
+        forwardVector,
+        direction
+      );
       const speed = 0.3;
       direction.x *= speed;
       direction.y *= speed;
       direction.z *= speed;
       this.flame.obj.applyForce(direction);
       this.spaceship.obj.applyForce(direction);
-
     }
     if (this.keysPressed.has("l")) {
-      const x = Math.random() * 20 - 10
-      const y = Math.random() * 10 - 5
-      this.mainScene?.killObject(this.spaceship.id)
-      this.mainScene?.killObject(this.flame.id)
+      const x = Math.random() * 20 - 10;
+      const y = Math.random() * 10 - 5;
+      this.currentScene.killObject(this.spaceship.id);
+      this.currentScene.killObject(this.flame.id);
 
       setTimeout(() => {
         this.spaceship.obj.setPosition(x, y, 0);
         this.flame.obj.setPosition(x, y, 0);
-        this.spaceship.id = this.mainScene?.addSceneMesh(this.spaceship.obj)!;
-        this.flame.id = this.mainScene?.addSceneMesh(this.flame.obj)!;
+        this.spaceship.id = this.currentScene.addSceneMesh(this.spaceship.obj)!;
+        this.flame.id = this.currentScene.addSceneMesh(this.flame.obj)!;
       }, 700);
-
     }
     if (this.keysPressed.has("k")) {
-      const bullet = new Bullet([this.spaceship.obj.position.x, this.spaceship.obj.position.y,this.spaceship.obj.position.z], [0.5,0.5,0.5], [0,0,0],this.spaceship.rotation );
-      bullet.boxCollider = [{x: -0.05, y: -0.05, z: 0}, {x: 0.05, y: 0.05, z: 5}]
+      console.log("xd");
+      const bullet = new Bullet(
+        [
+          this.spaceship.obj.position.x,
+          this.spaceship.obj.position.y,
+          this.spaceship.obj.position.z,
+        ],
+        [0.5, 0.5, 0.5],
+        [0, 0, 0],
+        this.spaceship.rotation
+      );
+      bullet.boxCollider = [
+        { x: -0.1, y: -0.1, z: 0 },
+        { x: 0.1, y: 0.1, z: -5 },
+      ];
       bullet.showBoxcollider = true;
-      const bulletID = this.mainScene?.addSceneMesh(bullet);
-      console.log(this.spaceship.obj.position)
+      const bulletID = this.currentScene.addSceneMesh(bullet);
+
+      if (this.currentScene == this.mainScene) {
+        this.asteroids.forEach((el, k) => {
+          console.log("test");
+          const ov = new BulletAsteroidOverlap(bullet, el, bulletID, k, this);
+          this.currentScene.addOverlap(ov);
+        });
+      }
+
+      console.log(this.spaceship.obj.position);
     }
-
-
   }
 
   handleKeyDown(e: KeyboardEvent) {
     this.keysPressed.add(e.key);
     if (e.key == "w") {
-      this.flame.obj.setPosition(this.spaceship.obj.position.x, this.spaceship.obj.position.y, this.spaceship.obj.position.z)
+      this.flame.obj.setPosition(
+        this.spaceship.obj.position.x,
+        this.spaceship.obj.position.y,
+        this.spaceship.obj.position.z
+      );
     }
     this.handleSpaceshipMove();
-
   }
 
   handleKeyUp(e: KeyboardEvent) {
     this.keysPressed.delete(e.key);
 
     if (e.key == "w") {
-      this.flame.obj.setPosition(1231231231, 123123123, 123123123)
-
+      this.flame.obj.setPosition(1231231231, 123123123, 123123123);
     }
   }
 
@@ -229,9 +311,13 @@ class MyGame extends Drake.Engine {
     const camera = new Drake.Camera(60, 0.1, 1000, [0, 0, -10], [0, 0, 1]);
 
     const mainScene = new Drake.Scene(this.width, this.height);
+    this.mainScene = mainScene;
 
-    const svgPath = "m 10 0 l 10 40 l -3 -5 l -14 0 l -3 5 z"
-    const mainSceneGUI = new GUI(this.getCanvas, this.getCanvas.getContext("2d")!);
+    const svgPath = "m 10 0 l 10 40 l -3 -5 l -14 0 l -3 5 z";
+    const mainSceneGUI = new GUI(
+      this.getCanvas,
+      this.getCanvas.getContext("2d")!
+    );
     mainSceneGUI.hideCursor = true;
     const resultText = new GUIText("00", 35, "Arial", "white", 100);
     const bestResultText = new GUIText("00", 35, "Arial", "white", 100);
@@ -256,7 +342,10 @@ class MyGame extends Drake.Engine {
     mainScene.setCamera(camera);
 
     const GUIScene = new Drake.Scene(this.width, this.height);
-    const GUISceneGUI = new GUI(this.getCanvas, this.getCanvas.getContext("2d")!);
+    const GUISceneGUI = new GUI(
+      this.getCanvas,
+      this.getCanvas.getContext("2d")!
+    );
     GUIScene.setCamera(camera);
 
     const t1 = new GUIText("Asteroids", 70, "monospace", "#fff", 700);
@@ -266,10 +355,10 @@ class MyGame extends Drake.Engine {
     t3.padding.top = 30;
     t3.padding.right = 90;
     t3.padding.left = 90;
-    t1.position.x = (this.width - t1.width)/2;
-    t1.position.y = this.height/2 - 100;
-    t2.position.x = (this.width - t1.width)/2;
-    t3.position.x = (this.width - t1.width)/2;
+    t1.position.x = (this.width - t1.width) / 2;
+    t1.position.y = this.height / 2 - 100;
+    t2.position.x = (this.width - t1.width) / 2;
+    t3.position.x = (this.width - t1.width) / 2;
 
     t3.position.y = t1.position.y + t1.height + 5 + t2.height + 30;
     t2.position.y = t1.position.y + t1.height + 5;
@@ -294,13 +383,14 @@ class MyGame extends Drake.Engine {
     document.addEventListener("keydown", this.handleKeyDown.bind(this));
     document.addEventListener("keyup", this.handleKeyUp.bind(this));
 
-    this.mainScene = GUIScene;
+
+    this.setCurrentScene(this.addScene(GUIScene));
 
     this.getCanvas.addEventListener("mousemove", (e: MouseEvent) => {
-      console.log(e.clientX);
-      console.log(e.clientY);
-      if (this.startButton && !this.startButton!.isCoordInElement(e.clientX, e.clientY)) {
-        console.log("xd2");
+      if (
+        this.startButton &&
+        !this.startButton!.isCoordInElement(e.clientX, e.clientY)
+      ) {
         const color = "white";
         this.startButton!.color = color;
         this.startButton!.border.bottom.color = color;
